0f65bcb54315b3f4c93666b3efd83b60d5f178a1
YmNIK13 <Zyfnt,trfryfd01yt>
2020-09-24 17:55:11 +0300

    add filter

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,7 +3,7 @@
 		<span v-bind:class="{done: todo.completed}">
 			<input type="checkbox" v-on:change="todo.completed = !todo.completed">
 			<strong>{{ index +1 }}</strong>
-			{{ todo.title }}
+			{{ todo.title | uppercase}}
 		</span>
 		<button class="rm" v-on:click="$emit('remove-todo', todo.id)">&times;</button>
 	</li>
@@ -17,7 +17,12 @@
                 required: true,
             },
             index : Number, // можно сходу указать тип
-        }
+        },
+	    filters: {
+            uppercase(value) {
+                return value.toUpperCase()
+            }
+	    },
     }
 </script>
 
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -6,11 +6,16 @@
 		<AddTodo
 				@add-todo="addTodo"
 		/>
+		<select v-model="filter">
+			<option value="all">All</option>
+			<option value="completed">Completed</option>
+			<option value="not-completed">Not Completed</option>
+		</select>
 		<hr/>
-		<Loader v-if="loading" />
+		<Loader v-if="loading"/>
 		<TodoList
-				v-else-if="todos.length"
-				v-bind:todos="todos"
+				v-else-if="filterTodos.length"
+				v-bind:todos="filterTodos"
 				@remove-todo="removeTodo"
 		/>
 
@@ -29,14 +34,15 @@
         data() {
             return {
                 todos: [],
-	            loading: true,
+                loading: true,
+                filter: 'all',
             }
         },
         mounted() {
             fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
                 .then(response => response.json())
                 .then(json => {
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         this.todos = json
                         this.loading = false
                     }, 500)
@@ -46,6 +52,23 @@
             TodoList,
             AddTodo,
             Loader,
+        },
+        // watch: {
+        //     filter(value) {
+        //     }
+        // },
+        computed: {
+            filterTodos() { // используем как переменную вместо массива todos
+                if (this.filter === 'all') {
+                    return this.todos
+                }
+                if (this.filter === 'completed') {
+                    return this.todos.filter(t => t.completed)
+                }
+                if (this.filter === 'not-completed') {
+                    return this.todos.filter(t => !t.completed)
+                }
+            }
         },
         methods: {
             removeTodo(id) {
