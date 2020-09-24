58d359f567e845412a31145452c47bfd096d08b3
YmNIK13 <Zyfnt,trfryfd01yt>
2020-09-24 17:22:40 +0300

    v-if-else

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -7,10 +7,14 @@
 				@add-todo="addTodo"
 		/>
 		<hr/>
+		<Loader v-if="loading" />
 		<TodoList
+				v-else-if="todos.length"
 				v-bind:todos="todos"
 				@remove-todo="removeTodo"
 		/>
+
+		<p v-else>No todo!</p>
 	</div>
 </template>
 
@@ -18,24 +22,30 @@
 <script>
     import TodoList from '@/components/TodoList'
     import AddTodo from '@/components/AddTodo'
+    import Loader from '@/components/Loader'
 
     export default {
         name: 'App',
         data() {
             return {
-                todos: []
+                todos: [],
+	            loading: true,
             }
         },
         mounted() {
             fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
                 .then(response => response.json())
                 .then(json => {
-                    this.todos = json
+                    setTimeout(()=>{
+                        this.todos = json
+                        this.loading = false
+                    }, 500)
                 })
         },
         components: {
             TodoList,
             AddTodo,
+            Loader,
         },
         methods: {
             removeTodo(id) {
